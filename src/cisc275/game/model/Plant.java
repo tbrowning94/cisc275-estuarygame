d9f4cf69466e65bc6f5e9216daafd030e3076482
Ryan Boucher <rboucher@udel.edu>
2015-10-28 15:50:31 -0400

    Edited Plant

--- src/cisc275/game/model/Plant.java
+++ src/cisc275/game/model/Plant.java
@@ -4,10 +4,11 @@ import java.awt.Point;
 
 public class Plant {
 	Point location;
-	int radius;
-	int efficiency;
-	int rate;
-	public Plant() {
+	int ranking;
+	private int radius;
+	private int efficiency;
+	private int rate;
+	public Plant(Point loc, int ranking) {
 		// TODO Auto-generated constructor stub
 	}
 	public void checkefficiency(Crab crabs){
