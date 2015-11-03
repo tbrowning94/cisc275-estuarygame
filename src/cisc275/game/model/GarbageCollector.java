e7eb9ad1780a2c87e63c62caeca97c70c868a5bc
rbruckel <rbruckel@udel.edu>
2015-11-02 23:44:51 -0500

    Updated garbage collector classes, created new class for random movement the crab and garbage collector can extend

--- src/cisc275/game/model/GarbageCollector.java
+++ src/cisc275/game/model/GarbageCollector.java
@@ -2,18 +2,52 @@ package cisc275.game.model;
 
 import java.awt.Point;
 
-public class GarbageCollector {
-	int ranking;
-	Point location;
-	private int radius;
-	private int efficiency;
-	private int rate;
-	int max;
-	int current;
-	public GarbageCollector(Point loc, int ranking) { //ranking determines radius, efficiency, rate
+/**
+ * @author rachelbruckel
+ *First level of Garbage Collectors. Players can add collectors to the game which then collect trash strewn about the field. Garbage Collectors move randomly
+ *and pick up garbage when it is within their radius. 
+ */
+public class GarbageCollector extends RandomMover {
+	Point location; //location of Garbage collector
+	static final int RADIUS = 0; //radius of area collect can pick up trash in
+	static final int RATE = 0; //rate at which collector can pick up trash - are we sure we still want this?
+	static final int BAG_CAPACITY = 0; //amount of garbage the collector can pick up before their bag is full
+	int currGarb; //the about of garbage currently in the collector's bag
+	
+	
+	/**
+	 * @param loc
+	 * Starting location determined by user input. Radius, rate, and bagCapacity are constant based on level of garbage collector.
+	 * currGarb always starts at 0. 
+	 */
+	public GarbageCollector(Point loc) { //ranking determines radius, efficiency, rate
 		// TODO Auto-generated constructor stub
 	}
+	
+	public void setLocation(Point location) {
+		
+	}
+	
+	public Point getLocation() {
+		return null;
+		
+	}
+
+	public void setCurrGarb(int currGarb) {
+	
+	}
+	
+	public int gerCurrGarb() {
+		return 0;
+	}
+	
+	/**
+	 * @return
+	 * compares currGarb to BAG_CAPACITY to determine if the bag is full. If it is the Garbage Collector will cease moving and can no longer pick up trash.
+	 */
 	public boolean checkfull() {
 		return false;
 	}
+	
+	
 }
---
+++ src/cisc275/game/model/RandomMover.java
@@ -0,0 +1,11 @@
+package cisc275.game.model;
+
+public abstract class RandomMover {
+	
+	/**
+	 * defines how objects extending this class will move. 
+	 */
+	public void moveRandomly() {
+		
+	}
+}
