5ea8f58adf70df1ae77a93cbb1f0bf1dde1c36dc
rbruckel <rbruckel@udel.edu>
2015-11-02 21:42:36 -0500

    Updated plant class, added two classes for higher level plants. Javadoc implemented, but no serializable

--- src/cisc275/game/model/Plant.java
+++ src/cisc275/game/model/Plant.java
@@ -2,25 +2,40 @@ package cisc275.game.model;
 
 import java.awt.Point;
 
-public class Plant {
+/**
+ * @author rachelbruckel
+ * First level of Plant. Every plant has to have a location, a radius, an efficiency and a way to decrease that efficiency. 
+ * As the level of the plant increases, the values for these parameters, and possibly the way efficiency is decreased, will change.
+ */
+ class Plant {
 	
 	//plant constants
-	static final int ranking = 1; //whether the plant is level one, two, or three (I think this might be easier to do as subclasses)
 	private static final int RADIUS = 0; //radius of the are the plant can filter/block water
 	static final int START_EFFICIENCY = 0; //efficiency before being attacked by crab
-	static final int START_RATE = 0; //rate before being attacked by crab
+	
 	
 	//plant variables
 	Point location; //where the plant was placed
-	private int efficiency; //I'm still not sure what the difference between this and rate is
-	private int rate; //rate at which the plant filters/blocks the water
+	private int efficiency; //the amount the plant can reduce the water's flow
 	
-	//ranking will determine radius efficiency and rate - how would we be able to call different constructors based on the ranking? I definitely think it should be multiple classes. 
+	
+	
+	/**
+	 * @param loc
+	 * Location will be determined by user action. All other attributes will have default values
+	 * based on the level of plant (see RADIUS and START_EFFICIENCY).
+	 */
 	public Plant(Point loc) { 
 		// TODO Auto-generated constructor stub
 	}
 	
-	//lowers the efficiency/rate of the plant (essentially a setter, will be called by crab) howMuch can be changed based on level if desired
+	
+	/**
+	 * @param howMuch
+	 * //lowers the efficiency of the plant (essentially a setter, will be called by crab) 
+	 * howMuch can be changed based on level, and can be factored differently for higher level plants,
+	 * making them more resilient to crabs.
+	 */
 	public void decreaseEfficiency(int howMuch){ 	
 	}
 }
---
+++ src/cisc275/game/model/Plant1.java
@@ -0,0 +1,29 @@
+package cisc275.game.model;
+
+import java.awt.Point;
+
+/**
+ * @author rachelbruckel
+ *Second level of Plant. The starting efficiency and radius of this plant will be higher than the first level of Plant. 
+ *Efficiency will also be decreased the less by crabs.
+ */
+public class Plant1 extends Plant {
+	//plant constants
+	private static final int RADIUS = 0; //radius of the are the plant can filter/block water
+	static final int START_EFFICIENCY = 0; //efficiency before being attacked by crab
+	
+	
+	public Plant1(Point loc) {
+		super(loc);
+		// TODO Auto-generated constructor stub
+	}
+	
+	/* (non-Javadoc)
+	 * @see cisc275.game.model.Plant#decreaseEfficiency(int)
+	 * doesn't necessarily need to be implemented again, there is just a possibility decreasing efficiency
+	 * will differ for each level of plant.
+	 */
+	@Override 	
+	public void decreaseEfficiency(int howMuch){ 	
+	}
+}
