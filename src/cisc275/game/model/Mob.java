7c1b9cf229d807ba51b0d5f4df7d4e3e8d40dc3a
Ryan Boucher <rboucher@udel.edu>
2015-11-03 15:52:03 -0500

    Mob

---
+++ src/cisc275/game/model/Mob.java
@@ -0,0 +1,16 @@
+package cisc275.game.model;
+
+import java.awt.Point;
+
+public class Mob {
+	Point location;
+	int radius;
+	public Mob() {
+		// TODO Auto-generated constructor stub
+	}
+	boolean checkProximity(){ 
+		return false;
+		
+	}
+
+}
