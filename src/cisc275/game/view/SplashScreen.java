e0769191b9cb753e39d6191b2799bdc5a1ccebf0
xXGp <tylerb@udel.edu>
2015-11-08 20:27:30 -0500

    Added listeners to the two buttons. I'm not sure where we want to do the adding of objects, but the button listeners in splash screen could be used to enable that functionality in the update method where we decide to do that

--- src/cisc275/game/view/SplashScreen.java
+++ src/cisc275/game/view/SplashScreen.java
@@ -7,6 +7,8 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -21,7 +23,10 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-public class SplashScreen extends JFrame{
+import cisc275.game.controller.PlaceObject;
+import cisc275.game.model.Plant;
+
+public class SplashScreen extends JFrame implements ActionListener{
 	private static final int WORLD_WIDTH = 1440;
 	private static final int WORLD_HEIGHT = 900;
 	private static final int SCALE = 1;
@@ -76,12 +81,16 @@ public class SplashScreen extends JFrame{
 		panel2.add(Box.createRigidArea(new Dimension(50, 0)));
 		
 		JButton button3 = new JButton("Plant");
+		button3.addActionListener(this);
+		button3.setActionCommand("Plant");
 //		  try {
 //			    Image img = ImageIO.read(new File("images/Grass.png"));
 //			    button.setIcon(new ImageIcon(img));
 //			  } catch (IOException ex) {
 //			  }
 		JButton button4 = new JButton("Garbage Collector");
+		button4.addActionListener(this);
+		button4.setActionCommand("Garbage Collector");
 		panel2.add(button3);
 		panel2.add(button4);
 		
@@ -104,5 +113,22 @@ public class SplashScreen extends JFrame{
 
         return null;
     }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String cmd = e.getActionCommand();
+		if(cmd.equals("Plant")){
+			//PlaceObject placeplant = new PlaceObject();
+			//we might not want this here, but this could invoke a call to update
+			//which could then place a plant at the next clicked location based on the 
+			//next mouse click that still has this button enabled
+        }
+		if(cmd.equals("Garbage Collector")){
+			//PlaceObject placegc = new PlaceObject();
+			//same thing here. maybe just set the button enable and then 
+			//invoke a call to update based on a click action listener which
+			//would give the location to place
+		}
+	}
 
 }
