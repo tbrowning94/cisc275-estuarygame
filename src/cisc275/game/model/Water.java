a15e4a1319958c0f57b1257ab91aacf7aff53f53
xXGp <tylerb@udel.edu>
2015-12-01 20:11:40 -0500

    Working on model aspects of water tiles, still need to figure out a pathing algorithm and add view related code

--- src/cisc275/game/model/Water.java
+++ src/cisc275/game/model/Water.java
@@ -8,6 +8,8 @@ import java.util.List;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
+import cisc275.game.view.GameView;
+
 /**
  * @author Nile
  *Generates, health, path and position of water and runoff particles
@@ -17,6 +19,8 @@ public class Water
 	Point location;
 	//ArrayList<Node> path = new ArrayList<Node>();
 	int health;
+	private double speed = 1.0;
+	private boolean removed;
 	int RunoffParticles;
 	Color runoffC;
 	private JButton wbutton;
@@ -29,6 +33,17 @@ public class Water
 		Color runoffC=RO;
 		this.wbutton = new JButton(wimg);
 		this.wbutton.putClientProperty("position", loc);
+		this.removed = false;
+	}
+	public Water(Point loc, int Health, int RP, Color RO, double speed) {
+		this.location = loc;
+		this.health = Health;
+		this.speed = speed;
+		this.RunoffParticles = RP; 
+		Color runoffC=RO;
+		this.wbutton = new JButton(wimg);
+		this.wbutton.putClientProperty("position", loc);
+		this.removed = false;
 	}
 	public int getHealth(){
 		return this.health;
@@ -66,8 +81,27 @@ public class Water
 	public void setrunoffC(Water health){
 	
 	}
+	public void decreaseHealth(int plantEff) {
+		this.health -= plantEff; // Health will be proportional to width, this
+								 // will be triggered by check proximity of the
+								 // plants radius
+		if (this.health <= 0) {
+			this.removed = true; // On the next update in game, removed all water tiles with removed set to true
+		}
+	}
 	public Color getrunoffC(){
 		return this.runoffC;
+	}
+	public void move() {
+		int x, y;
+		x = (int) this.location.getX();
+		y = (int) this.location.getY();
+		if (y < GameView.getWorldHeight() - 2*this.speed) {
+			y += 2*this.speed;
+		}
+		// else in water, update health, remove water
+		//TODO: come up with x algorithm, define water height of estuary
+		this.location.setLocation(x, y);
 	}
 	public String toString(){
 		return "[Water: location="+location+"RunoffParticles="+RunoffParticles
