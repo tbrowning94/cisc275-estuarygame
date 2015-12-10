4072fcc30b51dd22474cc378bacca685f3b4f295
xXGp <tylerb@udel.edu>
2015-12-10 14:38:11 -0500

    beginning to think this is going to take way too long..

--- src/cisc275/game/model/Level.java
+++ src/cisc275/game/model/Level.java
@@ -1,10 +1,16 @@
 package cisc275.game.model;
 
+import java.awt.Color;
+import java.awt.Point;
 import java.util.ArrayList;
+
+import javax.swing.JLabel;
 
 import cisc275.game.view.CrabView;
 import cisc275.game.view.GarbageCollectorView;
 import cisc275.game.view.PlantView;
+import cisc275.game.view.SplashScreen;
+import cisc275.game.view.ViewTemplate;
 
 public class Level {
 	int deletenum = -1; //with use of crabs
@@ -23,6 +29,12 @@ public class Level {
 	public int crabcount = 1;
 	public int watercount = 1;
 	public int garbcount = 1;
+	// we need to always have one fm separate from out list to track health
+	private static Fisherman mainFM = new Fisherman(new Point(ViewTemplate.scalex(1200),ViewTemplate.scaley(700)),new Point (ViewTemplate.scalex(500),ViewTemplate.scaley(700)), 1);
+	private static JLabel text = new JLabel();
+	int defualtX = ViewTemplate.scalex(10); // Change these to move text location
+	int defualtY = ViewTemplate.scalex(10);
+	
 	
 	public Level(int lvl) {
 		if(lvl == 0){
@@ -32,6 +44,17 @@ public class Level {
 
 	public void generateTutorial() {
 		// stall game, set intro text, spawn one water and enable
+		crabby = false;
+		removeAll();
+		text.setText("Place plants to stop water!");
+		text.setLocation(new Point(ViewTemplate.scalex(10), ViewTemplate.scaley(10)));
+		SplashScreen.getPanel2().add(text);
+		mainFM.setMoney(10);
+		SplashScreen.getPanel2().add(mainFM.getFLabel());
+		waterTiles.add(new Water(new Point (ViewTemplate.scalex(575),ViewTemplate.scaley(210)), ViewTemplate.scaley(48), 5, Color.BLUE, 1.0));
+		watercount += 1;
+		SplashScreen.paintwater();
+		crabby = true;
 		// the player to place a plant
 		// if they place it and decrease water size move to next state
 		// stall game, explain buffers and water effect, allow player to place buffer
@@ -46,5 +69,18 @@ public class Level {
 		// if garbage is removed stall the game, remind the player
 		// of fisherman, remove all and advance game state to level 1
 		
+	}
+	
+	public void removeAll() {
+		crabs.removeAll(crabs);
+		crabcount = 0;
+		crabss.removeAll(crabss);
+		plants.removeAll(plants);
+		waterTiles.removeAll(waterTiles);
+		watercount = 0;
+		fms.removeAll(fms);
+		garb.removeAll(garb);
+		garbcount = 0;
+		garbColl.removeAll(garbColl);
 	}
 }
